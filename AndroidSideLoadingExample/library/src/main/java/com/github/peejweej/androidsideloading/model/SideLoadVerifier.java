dabce576ac8574a73e7dd4ca3db0da782e08012a
PeeJWeeJ <pj.fechner@gmail.com>
2015-11-16 14:19:30 -0600

    Initial Commit

---
+++ AndroidSideLoadingExample/library/src/main/java/com/github/peejweej/androidsideloading/model/SideLoadVerifier.java
@@ -0,0 +1,14 @@
+package com.github.peejweej.androidsideloading.model;
+
+import android.net.Uri;
+
+import java.io.Serializable;
+
+/**
+ * Created by Fechner on 8/24/15.
+ */
+public abstract class SideLoadVerifier implements Serializable{
+
+    abstract public boolean fileIsValid(String file);
+    abstract public boolean fileIsValid(Uri file);
+}
